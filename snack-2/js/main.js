b115c3de7170ade49dfbbeeb262502146adeca9c
CyskoFR <franc.sko91@gmail.com>
2022-05-06 11:06:09 +0200

    add snack2

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -1,3 +1,17 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const green = document.getElementById("green");
+const red = document.getElementById("red");
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = document.createElement("p");
+    element.innerHTML = numbers[i];
+    if (numbers[i] % 2 === 0) {
+        green.append(element);
+    } else {
+        red.append(element);
+    }
+}
